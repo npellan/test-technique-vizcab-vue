8df9edf9735c528522857813a864bb160a68f28b
Nicolas <npellan@gmail.com>
2021-03-18 12:11:24 +0100

    🎉  first component with Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Employees />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Employees from './components/Employees.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Employees
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #16a085;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 1rem auto;
+  width: 700px;
 }
 </style>
---
+++ src/components/Employees.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <h1>My Employees</h1>
+        <div class="employees">
+            <div class="employee" v-for="employee in employees" :key="employee.id">
+                <h2 class="employee__name">{{employee.name}}</h2>
+                <p class="employee__job">{{employee.job_title}}</p>
+                <p class="employee__department">in {{employee.department}} department</p>
+                <p class="employee__company">@ {{employee.company_name}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Employees",
+  data() {
+    return {
+      employees: []
+    }
+  },
+  beforeMount(){
+    this.fetchEmployees();
+  },
+  methods: {
+    async fetchEmployees(){
+      const res = await fetch('https://my.api.mockaroo.com/employee.json?key=53b1b110');
+      const data = await res.json();
+      this.employees = data;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.employees {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-bottom: 3rem;
+}
+
+.employee {
+  width: 40%;
+  border-radius: 5px;
+  margin: 1rem;
+  padding: 1rem;
+  background-color: #ecf0f1;
+}
+
+@media screen and (max-width: 450px) {
+  .employee {
+    width: 100%;
+  }
+}
+
+.employee:hover {
+  transform: scale(1.05);
+  transition: .2s ease-in-out;
+}
+
+.employee__name {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: .5rem;
+}
+
+.employee__job {
+  font-size: 1.2rem;
+  font-style: italic;
+  margin-bottom: .5rem;
+}
+
+.employee__department {
+  margin-bottom: .5rem;
+}
+
+.employee__company {
+  text-transform: uppercase;
+}
+
+</style>
